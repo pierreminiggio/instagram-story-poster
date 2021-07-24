47a74085313944cc4e3678bcd8411882db2860fc
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-24 21:57:36 +0200

    add proxy

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ import sys
 
 args = sys.argv
 
-if len(args) != 4:
-    print('Use like this : python main.py <username> <password> <video_url>')
+if len(args) != 4 and len(args) != 5:
+    print('Use like this : python main.py <username> <password> <video_url> [proxy]')
     sys.exit()
 
 username = args[1]
 password = args[2]
 video_url = args[3]
+proxy = args[4] if 4 in args else None
 
 video_path = download_video_if_needed(video_url)
 
 print(video_path)
-story_id = upload_story(username, password, video_path)
+story_id = upload_story(username, password, video_path, proxy)
 os.remove(video_path)
 
 generated_thumbnail_path = video_path + '.jpg'
--- upload_story.py
+++ upload_story.py
@@ -1,7 +1,7 @@
 from instagrapi import Client
 
-def upload_story(username: str, password: str, video_path: str) -> int:
-    client = Client()
+def upload_story(username: str, password: str, video_path: str, proxy: str = None) -> int:
+    client = Client({}, proxy)
     client.login(username, password)
 
     story = client.video_upload_to_story(video_path)
