4750104a393d8f886a5843d79c673d470c691e60
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-07-24 23:18:55 +0200

    hide prints

--- upload_story.py
+++ upload_story.py
@@ -1,10 +1,12 @@
+from contextlib import redirect_stdout
 from instagrapi import Client
 
 def upload_story(username: str, password: str, video_path: str, proxy: str = None) -> int:
     client = Client({}, proxy)
-    client.logger.propagate = False
+    client.logger.disabled = True
     client.login(username, password)
 
-    story = client.video_upload_to_story(video_path)
+    with redirect_stdout(None):
+        story = client.video_upload_to_story(video_path)
 
     return story.id
