5c4b080fb52bbd17e27cdac84a681b84e69561b1
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-08-10 17:16:07 +0200

    test challenge

--- upload_story.py
+++ upload_story.py
@@ -1,9 +1,13 @@
 from contextlib import redirect_stdout
 from instagrapi import Client
 
+def challenge_code_handler(self, username: str, choice=None):
+    raise Exception('code !!!')
+
 def upload_story(username: str, password: str, video_path: str, proxy: str = None) -> int:
     client = Client({}, proxy)
     client.logger.disabled = True
+    client.challenge_code_handler = challenge_code_handler
     client.login(username, password)
 
     with redirect_stdout(None):
