70a84ff57a4de964844450ae8d1383cef535e009
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-08-15 02:17:40 +0200

    Update upload_story.py

--- upload_story.py
+++ upload_story.py
@@ -1,12 +1,13 @@
 from contextlib import redirect_stdout
 from instagrapi import Client
 
-def challenge_code_handler(self, username: str, choice=None):
-    raise Exception('code !!!')
-
-def upload_story(username: str, password: str, video_path: str, proxy: str = None) -> int:
+def upload_story(username: str, password: str, video_path: str, proxy: str = None, instagram_code_beggar_api_url: str = None, instagram_code_beggar_api_token: str = None) -> int:
     client = Client({}, proxy)
     client.logger.disabled = True
+    
+    def challenge_code_handler(self, username: str, choice=None):
+        raise Exception('code !!!')
+
     client.challenge_code_handler = challenge_code_handler
     client.login(username, password)
 
