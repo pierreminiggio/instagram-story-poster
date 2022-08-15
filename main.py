db143a57fb2e5d233a1ec89e816bd70bfe385d37
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-08-15 02:17:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import sys
 
 args = sys.argv
 
-if len(args) != 4 and len(args) != 5:
-    print('Use like this : python main.py <username> <password> <video_url> [proxy]')
+if len(args) != 4 and len(args) != 7:
+    print('Use like this : python main.py <username> <password> <video_url> [proxy] [instagram_code_beggar_api_url] [instagram_code_beggar_api_token]')
     sys.exit()
 
 username = args[1]
 password = args[2]
 video_url = args[3]
 proxy = args[4] if 4 in args else None
+instagram_code_beggar_api_url = args[5] if 5 in args else None
+instagram_code_beggar_api_token = args[6] if 6 in args else None
 
 video_path = download_video_if_needed(video_url, proxy)
 
@@ -28,7 +30,7 @@ for video_to_upload in videos_to_upload:
     story_id = None
     
     try:
-        story_id = upload_story(username, password, video_to_upload, proxy)
+        story_id = upload_story(username, password, video_to_upload, proxy, instagram_code_beggar_api_url, instagram_code_beggar_api_token)
     except Exception as e:
         exc_info = sys.exc_info()
         errors.append(''.join(traceback.format_exception(*exc_info)))
