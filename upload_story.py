530e9060ff259b161bf759f0252c78bb758983ba
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-08-15 02:24:28 +0200

    Update upload_story.py

--- upload_story.py
+++ upload_story.py
@@ -1,5 +1,6 @@
 from contextlib import redirect_stdout
 from instagrapi import Client
+import requests
 
 def upload_story(
     username: str,
@@ -13,7 +14,20 @@ def upload_story(
     client.logger.disabled = True
     
     def challenge_code_handler(self, username: str, choice=None):
-        raise Exception('code !!!')
+        if instagram_code_beggar_api_url == None AND instagram_code_beggar_api_token == None:
+            raise Exception('No code beggar configuration')
+        headers = {'Authorization': 'Bearer ' + instagram_code_beggar_api_token}
+        beggar_request = requests.get(instagram_code_beggar_api_url + username, headers=headers)
+        
+        if beggar_request.status_code != 200:
+            raise Exception('Beggar didn\'t return a 200')
+            
+        response_text = response.text
+        
+        if not response_text:
+            raise Exception('Beggar returned an empty response')
+            
+        return response_text
 
     client.challenge_code_handler = challenge_code_handler
     client.login(username, password)
